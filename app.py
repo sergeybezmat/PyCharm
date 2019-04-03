851e6076a8890a8075b1724981513656ddae742e
sergeybezmat <sbezm@yandex.ru>
2019-04-03 18:53:00 +0500

    Удалил основную часть программы, подключился к БД.

--- app.py
+++ app.py
@@ -1,54 +1,17 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from flask import render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://naucrm:naucrm@192.168.200.199/naumendb'
-app.debug = True
-db = SQLAlchemy(app)
+engine = create_engine("postgresql://naucrm:naucrm@172.16.200.199:5432/naumendb")
+connection = engine.connect()
 
+s = connection.execute('SELECT * FROM naumb_file_type').fetchall()
+s1 = connection.execute('SELECT login, status, sub_status, sum(duration) FROM ns_agent_sub_status_duration GROUP BY login, status, sub_status').fetchall()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 @app.route('/')
-@app.route('/index')
 def index():
-    myUser = User.query.all()
-    oneItem = User.query.filter_by(username='test2').firts()
-    return render_template('add_user.html', myUser=myUser, oneItem=oneItem)
-    ''' if request.args.get('username')==None:
-        aq = 'Пользователь'
-        sur = '!'
-    else:
-        if request.args.get('surname')==None:
-            aq = request.args.get('username')
-            sur = '!'
-        else:
-            aq = request.args.get('username')
-            aw = request.args.get('surname')
-            sur = {'surname': aw}
-    user = {'username': aq} '''
-
-    #return render_template('index.html', title='Home', user=user, sur=sur)
-
-
-@app.route('/post_user', methods=['GET'])
-def post_user():
-    user = User(request.args.get('username'), request.args.get('surname'))
-    db.session.add(user)
-    db.session.commit()
-    return redirect(url_for('index'))
-if __name__ == "__main__":
-    app.run()
-
+     return render_template('add_user.html', s=s, s1=s1)
 
