e89617b52199e46a8d10f9b93b0dd3c3afe78b63
sergeybezmat <sbezm@yandex.ru>
2019-04-05 17:31:52 +0500

    Заявки вчера и заявки сегодня заполняются из реальной БД

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from flask import render_template
 from flask_redis import Redis
+import datetime
+
+now = datetime.datetime.now()
+dataa = now.strftime("%Y-%m-%d")
+data_vhera = now - datetime.timedelta(days=1)
+vhera = data_vhera.strftime("%Y-%m-%d")
+
+
 
 app = Flask(__name__)
 engine = create_engine("postgresql://naucrm:naucrm@172.16.200.199:5432/naumendb")
@@ -12,6 +20,8 @@ app.config['REDIS_URL'] = 'redis://172.16.200.199:6379/db0'
 redis = Redis(app, 'REDIS')
 r = redis.hgetall("agents:state:offline")
 
+
+
 @app.route('/')
 def home():
      return render_template('home.html')
@@ -19,21 +29,32 @@ def home():
 
 @app.route('/index')
 def index():
-     l = request.args.get('login')
-     if l == None:
-          vvedi=1
-     else:
-            zaplog = connection.execute("SELECT namee, surname, plan, fakt, plan_mes, fakt_mes, za_vch, za_seg FROM TEST WHERE login='{0}'".format(l)).fetchone()
-            user = {'username': zaplog[0]}
-            sur = {'surname': zaplog[1]}
-            plan = {'p': zaplog[2]}
-            fakt = {'f': zaplog[3]}
-            plan_mes = {'p': zaplog[4]}
-            fakt_mes = {'f': zaplog[5]}
-            za_vch = {'z': zaplog[6]}
-            za_seg = {'z': zaplog[7]}
-            vvedi = 0
-     return render_template('index.html', title='Статистика', user=user, sur=sur, plan=plan, fakt=fakt, plan_mes=plan_mes, fakt_mes=fakt_mes, za_vch=za_vch, za_seg=za_seg, vvedi=vvedi)
+    l = request.args.get('login')
+    zv = connection.execute("SELECT is_processed FROM ns_inbound_call_data WHERE agent_connected = '{0}' and created_date = '{1}' ".format(l, dataa)) #Звонки за сегодня
+    chet_seg = 0
+    for i in zv:
+        if i[0] == True:
+            chet_seg += 1
+
+    zv1 = connection.execute("SELECT is_processed FROM ns_inbound_call_data WHERE agent_connected = '{0}' and created_date = '{1}' ".format(l, vhera))  # Звонки за вчера
+    chet_vch = 0
+    for i in zv1:
+        if i[0] == True:
+            chet_vch += 1
+
+
+
+
+    zaplog = connection.execute("SELECT namee, surname, plan, fakt, plan_mes, fakt_mes, za_vch, za_seg FROM TEST WHERE login='{0}'".format(l)).fetchone()
+    user = {'username': zaplog[0]}
+    sur = {'surname': zaplog[1]}
+    plan = {'p': zaplog[2]}
+    fakt = {'f': zaplog[3]}
+    plan_mes = {'p': zaplog[4]}
+    fakt_mes = {'f': zaplog[5]}
+    za_vch = {'z': chet_vch}
+    za_seg = {'z': chet_seg}
+    return render_template('index.html', title='Статистика', user=user, sur=sur, plan=plan, fakt=fakt, plan_mes=plan_mes, fakt_mes=fakt_mes, za_vch=za_vch, za_seg=za_seg)
 
 
 
